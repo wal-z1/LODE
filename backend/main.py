1ff4c0d25a8ff5314bdff0e588de36e676f0c8f4
Walid <ow_bouhenika@esi.dz>
2026-09-14 14:36:34 +0100

    update

--- backend/main.py
+++ backend/main.py
@@ -7,4 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "You have just accessed the root"}
 
-
+@app.get("/message/{message}")
+async def get_message(message: str):
+    return {"message": message}
